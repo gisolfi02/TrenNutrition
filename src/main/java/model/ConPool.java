8a2183050c31ffa8181489f2e7e94a101f7fac52
Andrea <114088374+gisolfi02@users.noreply.github.com>
2024-02-14 20:56:25 +0100

    Update ConPool.java

--- src/main/java/model/ConPool.java
+++ src/main/java/model/ConPool.java
@@ -16,7 +16,7 @@ public class ConPool {
             p.setUrl("jdbc:mysql://localhost:3306/db?serverTimezone=" + TimeZone.getDefault().getID());
             p.setDriverClassName("com.mysql.cj.jdbc.Driver");
             p.setUsername("root");
-            p.setPassword("Interista05.");
+            p.setPassword("Inserire qui la password");
             p.setMaxActive(100);
             p.setInitialSize(10);
             p.setMinIdle(10);
